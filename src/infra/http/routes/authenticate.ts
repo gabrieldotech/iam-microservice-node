39937443f1885ef228ed898fff0f4bb35f55c5bb
Gabriel <gabrieldotech@gmail.com>
2025-12-23 07:00:40 -0300

    refactor: implement global error handler and simplify route logic

--- src/infra/http/routes/authenticate.ts
+++ src/infra/http/routes/authenticate.ts
@@ -11,30 +11,26 @@ export async function authenticateRoute(app: FastifyInstance) {
 
     const { email, password } = authenticateBodySchema.parse(request.body);
 
-    try {
-      const authenticateUserUseCase = new AuthenticateUserUseCase();
+    const authenticateUserUseCase = new AuthenticateUserUseCase();
 
-      const { user } = await authenticateUserUseCase.execute({
-        email,
-        password_plain: password,
-      });
+    const { user } = await authenticateUserUseCase.execute({
+      email,
+      password_plain: password,
+    });
 
-      const token = await reply.jwtSign(
-        {},
-        {
-          sign: {
-            sub: user.id,
-            expiresIn: "7d",
-          },
-        }
-      );
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      }
+    );
 
-      return reply.status(200).send({
-        user,
-        token,
-      });
-    } catch (err) {
-      return reply.status(401).send({ message: "Invalid credentials." });
-    }
+    return reply.status(200).send({
+      user,
+      token,
+    });
   });
 }
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -3,6 +3,7 @@ import jwt from "@fastify/jwt";
 import { authenticateRoute } from "./routes/authenticate.js";
 import { getProfile } from "./routes/get-profile.js";
 import { createUser } from "./routes/create-user.js";
+import z from "zod";
 
 const app = fastify();
 
@@ -13,6 +14,29 @@ app.register(jwt, {
 app.register(authenticateRoute);
 app.register(getProfile);
 app.register(createUser);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof z.ZodError) {
+    return reply.status(400).send({
+      message: "Validation error.",
+      erros: z.treeifyError(error),
+    });
+  }
+
+  if (error instanceof Error && error.message === "User already exists.") {
+    return reply.status(409).send({ message: error.message });
+  }
+
+  if (error instanceof Error && error.message === "Invalid credentials.") {
+    return reply.status(401).send({ message: error.message });
+  }
+
+  if (process.env.NODE_ENV !== "production") {
+    console.error(error);
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Servidor rodando em http://localhost:3333");
